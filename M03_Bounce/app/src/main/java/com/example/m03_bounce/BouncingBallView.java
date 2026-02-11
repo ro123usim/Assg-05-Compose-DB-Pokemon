18908a81821f62d9d62f6b8dba6c6c8e54e2d253
ro123usim <w0517259@campus.nscc.ca>
2026-02-11 00:25:07 -0400

    edit the ball creation code

--- M03_Bounce/app/src/main/java/com/example/m03_bounce/BouncingBallView.java
+++ M03_Bounce/app/src/main/java/com/example/m03_bounce/BouncingBallView.java
@@ -15,150 +15,135 @@ import java.util.Random;
  */
 public class BouncingBallView extends View {
 
-    private ArrayList<Ball> balls = new ArrayList<Ball>(); // list of Balls
-    private Ball ball_1;  // use this to reference first ball in arraylist
+    private ArrayList<Ball> balls = new ArrayList<Ball>();
+    private Ball ball_1;
     private Box box;
 
-    // For touch inputs - previous touch (x, y)
     private float previousX;
     private float previousY;
+
+    Random rand = new Random();
 
     public BouncingBallView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         Log.v("BouncingBallView", "Constructor BouncingBallView");
 
-        // create the box
-        box = new Box(Color.BLACK);  // ARGB
+        // background color change (Challenge 1)
+        box = new Box(Color.BLUE);
 
-        //ball_1 = new Ball(Color.GREEN);
         balls.add(new Ball(Color.GREEN));
-        ball_1 = balls.get(0);  // points ball_1 to the first; (zero-ith) element of list
+        ball_1 = balls.get(0);
         Log.w("BouncingBallLog", "Just added a bouncing ball");
 
-        //ball_2 = new Ball(Color.CYAN);
         balls.add(new Ball(Color.CYAN));
         Log.w("BouncingBallLog", "Just added another bouncing ball");
 
-        // To enable keypad
         this.setFocusable(true);
         this.requestFocus();
-        // To enable touch mode
         this.setFocusableInTouchMode(true);
     }
 
-    // Called back to draw the view. Also called after invalidate().
     @Override
     protected void onDraw(Canvas canvas) {
 
         Log.v("BouncingBallView", "onDraw");
 
-        // Draw the components
         box.draw(canvas);
-        //canvas.drawARGB(0,25,25,25);
-        //canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
 
+        for (Ball b : balls) {
+            b.draw(canvas);
+            b.moveWithCollisionDetection(box);
+        }
 
-            for (Ball b : balls) {
-                b.draw(canvas);  //draw each ball in the list
-                b.moveWithCollisionDetection(box);  // Update the position of the ball
-
-//                /// check if ball is in this square
-//                float rect_x1 = 100;
-//                float rect_x2 = 200;
-//                float rect_y1 = 250;
-//                float rect_y2 = 350;
-//
-//                //check if x1-x2 is small AND y1-y2 is small
-//                if ((b.x>rect_x1) &&
-//                        (b.x<rect_x2) &&
-//                        (b.y>rect_y1) &&
-//                        (b.y<rect_y2)) {
-//                    // Hit .. double speed
-//                    b.speedX = 2* b.speedX;
-//                    b.speedY = 2* b.speedY;
-//                    Log.w("Bounce", "HIT!");
-//                }
-
-            }
-
-        // Delay on UI thread causes big problems!
-        // Simulates doing busy work or waits on UI (DB connections, Network I/O, ....)
-        //  I/Choreographer? Skipped 64 frames!  The application may be doing too much work on its main thread.
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//        }
-
-        // Check what happens if you draw the box last
-        //box.draw(canvas);
-
-        // Check what happens if you don't call invalidate (redraw only when stopped-started)
-        // Force a re-draw
+        // keeps animation running
         this.invalidate();
     }
 
-    // Called back when the view is first created or its size changes.
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        // Set the movement bounds for the ball
         box.set(0, 0, w, h);
         Log.w("BouncingBallLog", "onSizeChanged w=" + w + " h=" + h);
     }
 
-    // Touch-input handler
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         float currentX = event.getX();
         float currentY = event.getY();
         float deltaX, deltaY;
         float scalingFactor = 5.0f / ((box.xMax > box.yMax) ? box.yMax : box.xMax);
+
         switch (event.getAction()) {
+
             case MotionEvent.ACTION_MOVE:
-                // Modify rotational angles according to movement
+
                 deltaX = currentX - previousX;
                 deltaY = currentY - previousY;
+
                 ball_1.speedX += deltaX * scalingFactor;
                 ball_1.speedY += deltaY * scalingFactor;
-                //Log.w("BouncingBallLog", " Xspeed=" + ball_1.speedX + " Yspeed=" + ball_1.speedY);
-                Log.w("BouncingBallLog", "x,y= " + previousX + " ," + previousY + "  Xdiff=" + deltaX + " Ydiff=" + deltaY);
-                balls.add(new Ball(Color.BLUE, previousX, previousY, deltaX, deltaY));  // add ball at every touch event
 
-                // A way to clear list when too many balls
+                Log.w("BouncingBallLog",
+                        "x,y= " + previousX + " ," + previousY +
+                                "  Xdiff=" + deltaX + " Ydiff=" + deltaY);
+
+                // random color
+                int r = rand.nextInt(256);
+                int g = rand.nextInt(256);
+                int b = rand.nextInt(256);
+                int randomColor = Color.rgb(r, g, b);
+
+                // SUPER-FAST / SUPER-SLOW BALLS
+                float speedMultiplier;
+
+                if (Math.abs(deltaX) + Math.abs(deltaY) > 50) {
+                    speedMultiplier = 3.0f;   // fast swipe
+                    Log.d("BALL_SPEED", "FAST BALL");
+                } else {
+                    speedMultiplier = 0.5f;   // slow swipe
+                    Log.d("BALL_SPEED", "SLOW BALL");
+                }
+
+                balls.add(new Ball(
+                        randomColor,
+                        previousX,
+                        previousY,
+                        deltaX * speedMultiplier,
+                        deltaY * speedMultiplier
+                ));
+
                 if (balls.size() > 20) {
-                    // leave first ball, remove the rest
                     Log.v("BouncingBallLog", "too many balls, clear back to 1");
                     balls.clear();
                     balls.add(new Ball(Color.RED));
-                    ball_1 = balls.get(0);  // points ball_1 to the first (zero-ith) element of list
+                    ball_1 = balls.get(0);
                 }
-
         }
-        // Save current x, y
+
         previousX = currentX;
         previousY = currentY;
 
-        // Try this (remove other invalidate(); )
-        //this.invalidate();
-
-        return true;  // Event handled
+        return true;
     }
 
-    Random rand = new Random();
-    // called when button is pressed
-    public void RussButtonPressed() {
-        Log.d("BouncingBallView  BUTTON", "User tapped the  button ... VIEW");
 
-        //get half of the width and height as we are working with a circle
+    public void RussButtonPressed() {
+        Log.d("BouncingBallView BUTTON", "User tapped the button ... VIEW");
+
         int viewWidth = this.getMeasuredWidth();
         int viewHeight = this.getMeasuredHeight();
 
-        // make random x,y, velocity
         int x = rand.nextInt(viewWidth);
         int y = rand.nextInt(viewHeight);
         int dx = rand.nextInt(50);
         int dy = rand.nextInt(20);
 
-        balls.add(new Ball(Color.RED, x, y, dx, dy));  // add ball at every touch event
+        // also random color for button-created balls
+        int r = rand.nextInt(256);
+        int g = rand.nextInt(256);
+        int b = rand.nextInt(256);
+        int randomColor = Color.rgb(r, g, b);
+
+        balls.add(new Ball(randomColor, x, y, dx, dy));
     }
 }
